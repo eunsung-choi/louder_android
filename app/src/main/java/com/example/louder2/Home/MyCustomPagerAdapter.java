8711ea9ffbac882126db622eec40981174cf7fb1
eunsung-choi <dmstjd2910000@naver.com>
2022-11-24 01:25:19 +0900

    viewpager2 마무리

---
+++ app/src/main/java/com/example/louder2/Home/MyCustomPagerAdapter.java
@@ -0,0 +1,61 @@
+package com.example.louder2.Home;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import androidx.viewpager.widget.PagerAdapter;
+
+import com.example.louder2.R;
+
+public class MyCustomPagerAdapter extends PagerAdapter {
+    Context context;
+    int images[];
+    LayoutInflater layoutInflater;
+
+
+    public MyCustomPagerAdapter(Context context, int images[]) {
+        this.context = context;
+        this.images = images;
+        layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return images.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(View view, Object object) {
+        return view == object;
+    }
+
+    @Override
+    public Object instantiateItem(ViewGroup container, final int position) {
+        View itemView = layoutInflater.inflate(R.layout.home_page1, container, false);
+
+        ImageView imageView = (ImageView) itemView.findViewById(R.id.image);
+        imageView.setImageResource(images[position]);
+
+        container.addView(itemView);
+
+//listening to image click
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "you clicked image " + position, Toast.LENGTH_LONG).show();
+            }
+        });
+
+        return itemView;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
+        container.removeView((LinearLayout) object);
+    }
+}
